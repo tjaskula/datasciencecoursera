10789fa037a23513c2ff3a8dbf205a9deeab483a
Thomas Jaskula <thomasv1000@hotmail.fr>
2016-01-26 19:02:17 +0100

    Working on merging files

---
+++ Assignment - Getting and Cleaning Data Course Project/mergedatasets.R
@@ -0,0 +1,5 @@
+# Reads two files from a given paths @trainPath and @testPath,
+# Merges them together and writes back a merged file into the @toPath
+merge <- function(trainPath, testPath, toPath) {
+  # df <- read.csv("./datasets/UCI HAR Dataset/train/X_train.txt", sep = " ", header = FALSE, colClasses = "character")
+}
---
+++ Assignment - Getting and Cleaning Data Course Project/run_analysis.R
@@ -0,0 +1,10 @@
+# Main entry point to run the whole analysis.
+run_analysis <- function() {
+  
+  # references sub scripts for specific behavior.
+  if(!exists("mergeTrainingTest", mode="function")) source("mergedatasets.R")
+  
+  # call to merge between training set and test set.
+  
+  
+}
